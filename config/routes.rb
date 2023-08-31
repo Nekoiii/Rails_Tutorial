af8acac86f8176fa170c516d35f1fc0f2992ad96
Nekoiii <10nekosa@gmail.com>
2023-08-31 10:44:01 +0900

    added Procfile (for heroku)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   
+  resources :microposts
+  resources :users
+  
   get  "/help", to: "static_pages#help"
   get  "/about", to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
-  resources :microposts
-  resources :users
   get  "/signup",  to: "users#new"
   get '/users/new', to: 'users#new', as: 'users_new'
 end
