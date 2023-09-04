7fb93d6ab84723d72789e3aacdf96364762c9d96
Nekoiii <10nekosa@gmail.com>
2023-09-04 09:38:57 +0900

    doing user signup

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+users=[]
+20.times do |i|
+  name  = "User #{i+1}"
+  email = "user-#{i+1}@mail.com"
+  password = "password"
+  user=User.create!(name:  name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+              #  activated: true,
+              #  activated_at: Time.zone.now
+              )
+  users << user
+end
