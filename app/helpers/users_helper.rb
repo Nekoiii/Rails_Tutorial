6b633b0bcf78f08e405719f0c41f3182fa36d668
Nekoiii <10nekosa@gmail.com>
2023-09-20 07:44:23 +0900

    xxxx

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,11 @@
 
 module UsersHelper
   def gravatar_for(user, size: 80)
-    gravatar_id  = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    # gravatar_id  = Digest::MD5.hexdigest(user.email.downcase)
+    # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    # image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    default_avatar_path=asset_path('avatar-1.jpg')
+    user.avatar.attached? ? url_for(user.avatar) : default_avatar_path
   end
+
 end
