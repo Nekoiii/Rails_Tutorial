64eb616223921776cb263ce85df513ffbca6791e
Nekoiii <10nekosa@gmail.com>
2023-09-20 19:41:58 +0900

    modifing user avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,12 +2,15 @@
 
 module UsersHelper
   def gravatar_for(user, size: 80)
-    # gravatar_id  = Digest::MD5.hexdigest(user.email.downcase)
-    # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    # image_tag(gravatar_url, alt: user.name, class: 'gravatar')
-    default_avatar_path=asset_path('avatar-1.jpg')
-    avatar_url=user.avatar.attached? ? url_for(user.avatar) : default_avatar_path
-    image_tag(avatar_url, alt: user.name, class: 'user_avatar')
+    gravatar_id  = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
+  
+  def avatar_for(user)
+    default_avatar_path=asset_path('avatar-1.jpg')
+    user.avatar.attached? ? url_for(user.avatar) : default_avatar_path
+  end
+
 
 end
