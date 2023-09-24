5b015eda59887c00ab51ba5a1a0025477a4dff0b
Nekoiii <10nekosa@gmail.com>
2023-09-25 07:23:12 +0900

    Remove ignored files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,42 +1,25 @@
+# frozen_string_literal: true
 
-# RSpec describe/context/example/it/before/subject... : 
+# RSpec describe/context/example/it/before/subject... :
 # https://zenn.dev/igaiga/books/rails-practice-note/viewer/rails_rspec_workshop
-"""
-require 'rails_helper'
-RSpec.describe Book, type: :model do
-  describe '#.メソッド名' do
-    context '○○なとき' do
-      it '○○なこと' do end
-      it '○○なこと' do end
-    end
-    context '○○なとき' do
-      it '○○なこと' do end
-    end
-  end
-  describe '#.メソッド名' do ... end
-end
-"""
 
 require 'rails_helper'
 
-
-# 'RSpec.describe' is equals 'describe', 'RSpec.' here is just used to make it  
+# 'RSpec.describe' is equals 'describe', 'RSpec.' here is just used to make it
 # clearer that it's an RSpec description block and not a regular Ruby method.
 RSpec.describe User, type: :model do
   before do
-    @user=User.new(
+    @user = described_class.new(
       name: 'Example User', email: 'user@example.com',
       password: 'aaaaaa', password_confirmation: 'aaaaaa'
     )
   end
 
   describe 'validations' do
-
-    it 'should be valid' do
+    it 'is valid' do
       expect(@user).to be_valid
     end
 
-    
     context 'when name' do
       it 'gets user name' do
         expect(@user.name).to eq 'Example User'
@@ -61,34 +44,32 @@ RSpec.describe User, type: :model do
 
       it 'is valid' do
         valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-          first.last@foo.jp alice+bob@baz.cn]
+                             first.last@foo.jp alice+bob@baz.cn]
         valid_addresses.each do |valid_address|
           @user.email = valid_address
           expect(@user).to be_valid
         end
       end
 
-      it 'should be unique' do
+      it 'is unique' do
         duplicate_user = @user.dup
         @user.save
         expect(duplicate_user).not_to be_valid
       end
 
-      it 'should be saved as lowercase' do
+      it 'is saved as lowercase' do
         mixed_case_email = 'Foo@ExAMPle.CoM'
         @user.email = mixed_case_email
         @user.save
         # .reload: get updated data from db
         expect(@user.reload.email).to eq mixed_case_email.downcase
       end
-
     end
 
-
     context 'when password' do
-      it 'should not be blank' do
+      it 'is not blank' do
         @user.password = @user.password_confirmation = ' ' * User::PASSWORD_LENGTH_MIN
-        expect(@user).not_to be_valid  
+        expect(@user).not_to be_valid
       end
 
       it 'is too short' do
@@ -100,32 +81,31 @@ RSpec.describe User, type: :model do
 
   describe 'methods' do
     it 'authenticated? should return false for a user with nil digest' do
-      expect(@user.authenticated?(:remember, '')).to be_falsey
+      expect(@user).not_to be_authenticated(:remember, '')
     end
 
     it 'associated microposts should be destroyed' do
       @user.save
       @user.microposts.create!(content: 'Lorem ipsum')
-      expect { @user.destroy }.to change { Micropost.count }.by(-1)
+      expect { @user.destroy }.to change(Micropost, :count).by(-1)
     end
 
     context 'when user follows and unfollows' do
       it 'another user' do
         test_user_1 = create(:test_user_3)
         test_user_2 = create(:test_user_2)
-        expect(test_user_1.following?(test_user_2)).to be_falsey
+        expect(test_user_1).not_to be_following(test_user_2)
         test_user_1.follow(test_user_2)
-        expect(test_user_1.following?(test_user_2)).to be_truthy
+        expect(test_user_1).to be_following(test_user_2)
         expect(test_user_2.followers).to include(test_user_1)
         test_user_1.unfollow(test_user_2)
-        expect(test_user_1.following?(test_user_2)).to be_falsey
+        expect(test_user_1).not_to be_following(test_user_2)
       end
     end
-
   end
 
   describe 'feeds' do
-    it 'should have the right posts' do
+    it 'has the right posts' do
       test_user_1 = create(:test_user_1)
       test_user_3 = create(:test_user_3)
       test_user_4 = create(:test_user_4)
@@ -146,6 +126,4 @@ RSpec.describe User, type: :model do
       end
     end
   end
-
-
 end
